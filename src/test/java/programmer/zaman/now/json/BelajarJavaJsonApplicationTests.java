5808ee5e33aa097c5e06c6a89253ea8e7407bb44
sonypiay <pyscho30@gmail.com>
2025-02-15 18:20:09 +0700

    init project

---
+++ src/test/java/programmer/zaman/now/json/BelajarJavaJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package programmer.zaman.now.json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
